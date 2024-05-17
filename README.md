a56f5c121bb4a96e937d6099d3e76ff03d08e781
lugq <gqianglu@outlook.com>
2024-05-17 16:55:58 +0800

    add the func to fullful full markdown files to homepage

--- README.md
+++ README.md
@@ -5,4 +5,43 @@ This repo is a daily learning path to enhance the ability of software engineerin
 
 For each folder is a sub-materials, will try to add a functionality that will match each of the folder of the content here, so that for later will get be easier to read. Some of them are based on LLM models output like ChatGPT.
 
-## TODO: add index for each of them.
+## python
+
+[pandasql](./python/python_labs/pandasql.md)
+
+
+## big_data
+
+[zeppelin_basic](./big_data/framework/zeppelin/zeppelin_basic.md)
+
+[spark_starter](./big_data/framework/spark/spark_starter.md)
+
+[spark_tuning](./big_data/framework/spark/spark_tuning.md)
+
+[spark_structure_streaming](./big_data/framework/spark/spark_structure_streaming.md)
+
+[spark_basic](./big_data/framework/spark/spark_basic.md)
+
+[system_build_things](./big_data/concept_core/distributed_core/system_build_things.md)
+
+[distribute_for_bigdata](./big_data/concept_core/distributed_core/distribute_for_bigdata.md)
+
+[big_data_core](./big_data/concept_core/big_data/big_data_core.md)
+
+[CICD](./big_data/concept_core/micro_service/CICD.md)
+
+[concept](./big_data/concept_core/data_lake/concept.md)
+
+[hudi](./big_data/concept_core/data_lake/hudi.md)
+
+
+## ai
+
+[kv_cache](./ai/LLM/optimization_for_llm/kv_cache.md)
+
+[page_attention](./ai/LLM/optimization_for_llm/page_attention.md)
+
+[ai_agent_senario](./ai/LLM/ai_agent/ai_agent_senario.md)
+
+[recommedation](./ai/real_world_problems/recommedation.md)
+
---
+++ convert_notebook_materials_to_readme.py
@@ -0,0 +1,60 @@
+import os
+
+def get_md_files_dict(directory):
+    """
+    This function will iteratively loop through the full current folder, with each folder will be a title as parent folder,
+    iterative get each folder as sub-parent folder, for these folders are formated as markdown format.
+    For the .md files should be a tuple, with .md file name as the first value, the .md file path as second value.
+    
+    The return value is a string that could used in markdown file.
+    
+    
+    Parameters:
+    directory (str): The directory to scan for MD files.
+    """
+    # Initialize an empty dictionary
+    md_files_dict = {}
+    markdown_str = ""
+    seen_folder = set()
+
+    # Iterate through all files in the given directory
+    for root, dirs, files in os.walk(directory):
+        if not dirs:
+            folder_path = os.path.relpath(root)
+            files = [x for x in files if x.endswith('.md')]
+            if not files:
+                continue
+            root_path_name = root.split('/')[1]
+            if root_path_name not in seen_folder:
+                seen_folder.add(root_path_name)
+                markdown_str += '\n## {}\n\n'.format(root_path_name)
+            
+            md_files_dict[folder_path] = files
+            for i, f in enumerate(files):
+                f_name = f.split('.')[0]
+                file_path = os.path.join(folder_path, f)
+                markdown_str += "[{}](./{})\n\n".format(f_name, file_path)
+            
+    return markdown_str
+
+# Example usage:
+directory_path = '.'
+markdown_str = get_md_files_dict(directory_path)
+
+
+markdown_prefix = """# materials_for_learning
+Materials summary during daily learning
+
+This repo is a daily learning path to enhance the ability of software engineering, including basic computer info, distributed, AI, cloud, python etcs.
+
+For each folder is a sub-materials, will try to add a functionality that will match each of the folder of the content here, so that for later will get be easier to read. Some of them are based on LLM models output like ChatGPT.
+"""
+
+
+final_markdown = markdown_prefix + markdown_str
+
+print(final_markdown)
+
+
+with open('./README.md', 'w') as f:
+    f.write(final_markdown)
